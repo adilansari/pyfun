8df678d269040b58901c2aadfc72626c23e8be1b
Adil Ansari <ansari.adil20@gmail.com>
2017-03-07 23:28:23 -0800

    new converter coming to shape

--- stocks_parser/convert_statement.py
+++ stocks_parser/convert_statement.py
@@ -7,6 +7,27 @@ input_xlsx_file = 'input/profit_loss_statement.xlsx'
 def convert_xls_to_csv(xlsfile, csvfile):
     os.system('in2csv {} > {}'.format(xlsfile, csvfile))
 
+def build_data_store(csvfile):
+    clean_buffer = _clean_csv(csvfile)
+    print clean_buffer
+
+    # convert this buffer to a dict
+    # extract data from this buffer
+    # create both output files
+
+def _clean_csv(csvfile):
+    def _is_useful_data(line):
+        if 'Gain' in line and 'Charges' in line:
+            return True
+        # if a key from symbol map exists here
+        # return True
+        return False
+
+    with open(csvfile, 'r') as lines:
+        return filter(_is_useful_data, lines)
 
 if __name__ == '__main__':
     convert_xls_to_csv(input_xlsx_file, input_csv_file)
+    build_data_store(input_csv_file)
+
+# build a name => Symbol map iin conf.py
